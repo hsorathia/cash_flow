4d4666fd8587525e5118c9546d31317edab0a3bb
Christine <christinemnguyen@users.noreply.github.com>
2019-11-04 16:36:33 -0800

    login / signup pages

--- app/routes.py
+++ app/routes.py
@@ -5,28 +5,30 @@ from app import app
 from app.forms import loginForm
 from app.forms import signUp
 import os
-image_folder = os.path.join('static','image')
+image_folder = os.path.join('static', 'image')
 app.config['UPLOAD_FOLDER'] = image_folder
 @app.route('/')
+@app.route('/home')
 def home():
-    filename_logo1 = os.path.join(app.config['UPLOAD_FOLDER'],'logo1.jpg')
-    #filename_logo2 = os.path.join(app.config['UPLOAD_FOLDER'],'logo2.jpg')
-    #filename_logo3 = os.path.join(app.config['UPLOAD_FOLDER'],'logo3.png')
-    return render_template('home.html',logo1 = filename_logo1 )
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
+    filename_logo1 = os.path.join(app.config['UPLOAD_FOLDER'], 'logo1.jpg')
+    filename_logo2 = os.path.join(app.config['UPLOAD_FOLDER'], 'logo2.png')
+    filename_logo3 = os.path.join(app.config['UPLOAD_FOLDER'], 'logo3.png')
+    return render_template('home.html', logo1=filename_logo1, logo2=filename_logo2, logo3=filename_logo3)
 
 
 @app.route('/login', methods=['GET', 'POST'])
-def Login():
+def login():
     form = loginForm()
     if form.validate_on_submit():
         flash('Login requested for user {}, rememberMe={}'.format(
             form.username.data, form.rememberMe.data))
-        return redirect(url_for('index'))
+        return redirect(url_for('home'))
     return render_template('login.html',  title='Sign In', form=form)
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def register():
+    form = signUp
 
 
 @app.route('/output')
@@ -37,7 +39,8 @@ def output():
     return render_template('output.html', valid=valid)
 
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 # @app.route('/register', methods=["Get", "Post"])
